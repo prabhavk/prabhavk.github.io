bf5692a029ce26030370e810ced444c984b70498
Prabhav Kalaghatgi <prabhavk@gmail.com>
2025-08-18 07:09:16 +0530

    dropping type annotation

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,13 @@
-import type { NextConfig } from "next";
-
-const devExperimental =
-  process.env.NODE_ENV === "development"
+// next.config.ts
+const nextConfig = {
+  output: "export",
+  ...(process.env.NODE_ENV === "development"
     ? {
         experimental: {
-          // allow your LAN device to open the dev server
           allowedDevOrigins: ["http://192.168.29.107:3000"],
         },
       }
-    : {};
-
-const nextConfig: NextConfig = {
-  output: "export",
-  ...devExperimental,
+    : {}),
 };
 
 export default nextConfig;
