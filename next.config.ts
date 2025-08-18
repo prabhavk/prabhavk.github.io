2bdad91261f2ea139a78fd0f0b398017d4782d25
Prabhav Kalaghatgi <prabhavk@gmail.com>
2025-08-18 06:19:50 +0530

    updated next.config.ts to be compatible with cross origin request

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+
+  experimental: {
+    // Explicitly allow LAN dev access (update IP if needed)
+    allowedDevOrigins: ["http://192.168.29.107:3000"],
+  },
 };
 
 export default nextConfig;
